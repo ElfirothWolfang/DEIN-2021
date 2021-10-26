a32664fceb8b0fdf4ee16e871f78358107300e9e
CarlosCarbajoRua <ccarbr09021993@gmail.com>
2021-10-26 17:49:34 +0200

    20211026 - Finalizado ejemplos de ventanas.

--- src/main/java/tutorialVentanas/VentanaNumeritos.java
+++ src/main/java/tutorialVentanas/VentanaNumeritos.java
@@ -17,9 +17,15 @@ public class VentanaNumeritos extends Frame implements WindowListener{
         setSize(400,400);
         setTitle("Ventana");
         setVisible(true);
-        Panel main = new Panel(new GridLayout(4,3));
-        main.setSize(400,400);
+        Panel main = new Panel(new GridBagLayout());
+        Panel displayPanel = new Panel(new FlowLayout());
+        Panel panelNumeros = new Panel(new GridLayout(4,3));
+        Panel panelOperaciones = new Panel (new GridLayout (4,1));
         this.add(main);
+        
+        TextField display = new TextField();
+        display.setColumns(10);
+        displayPanel.add (display);
         
         String [] numeros = {
            "7", "8", "9",
@@ -28,7 +34,13 @@ public class VentanaNumeritos extends Frame implements WindowListener{
            "0", ".", "EXP"
         };
         
+        String [] operaciones = {
+            "/", "*", "-", "+"
+        };
+        
         Button botones[] = new Button[12];
+        Button botonesOperaciones [] = new Button [4];
+        
         for (int i = 0; i < numeros.length; i++) {
             botones[i] = new Button(numeros[i]);
             final int x = i;
@@ -38,9 +50,45 @@ public class VentanaNumeritos extends Frame implements WindowListener{
                     System.out.println("He pulsado " + x);
                 }
             }); 
-            main.add(botones[i]);
+            panelNumeros.add(botones[i]);
             
         }
+        
+        for (int i = 0; i < operaciones.length; i++) {
+            botonesOperaciones[i] = new Button (operaciones[i]);
+            final int x = i;
+            botonesOperaciones[i].addActionListener(new ActionListener () {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    System.out.println("He pulsado " + x);
+                }
+            } );
+            panelOperaciones.add(botonesOperaciones[i]);
+            
+        }
+        
+        
+        
+        
+        GridBagConstraints gbc = new GridBagConstraints();
+        
+        gbc.gridheight = 1;
+        gbc.gridwidth = 4;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        main.add(displayPanel, gbc);
+        
+        gbc.gridheight = 1;
+        gbc.gridwidth = 3;
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        main.add(panelNumeros, gbc);
+        
+        gbc.gridheight = 1;
+        gbc.gridwidth = 1;
+        gbc.gridx = 3;
+        gbc.gridy = 1;
+        main.add(panelOperaciones, gbc);
         
         addWindowListener(this);
     }
